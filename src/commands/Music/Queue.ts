9dad9049f90f7287f5587bfc0baf808e8a74db21
Ruzie <luhamailbox@gmail.com>
2022-05-07 12:00:06 -0700

    reverted

--- src/commands/Music/Queue.ts
+++ src/commands/Music/Queue.ts
@@ -1,346 +1,57 @@
-import {
-  ComponentInteraction,
-  Constants,
-  type Message,
-  type Interaction,
-  type User,
-} from "eris";
-import prettyMs from "pretty-ms";
-import {
-  nowplayingButtons,
-  nowplayingModifier,
-  trackStartButtons,
-  trackStartModifier,
-} from "../../utils/SetButtons";
-import Event from "../../structures/Event";
-import ruqa from "../../index";
+import { type Client, Constants, type Message, type User } from "eris";
+import buildMsg from "../../functions/BuildMsg";
+import isPlayerActive from "../../functions/IsPlayerActive";
+import notInVC from "../../functions/NotInVC";
+import Command from "../../structures/Command";
 import RichEmbed from "../../utils/RichEmbed";
-import { buildProgressBar } from "../../utils/Util";
-import onlyAllowedRequester from "../../functions/OnlyAllowedReq";
+import { queueActiveButtons } from "../../utils/SetButtons";
+import { makeChunk } from "../../utils/Util";
 
-export default new Event(
-  "interactionCreate",
-  async (interaction: Interaction) => {
-    await (interaction as ComponentInteraction).acknowledge().catch(() => {});
+export default new Command({
+  name: "queue",
+  description: "Shows the upcoming queue",
+  aliases: ["q"],
+  category: "Music",
+  isDisabled: false,
 
-    if (interaction instanceof ComponentInteraction) {
-      const player = ruqa.audio.players.get(interaction.guildID!);
-      let msg: Message;
-      switch (interaction.data.custom_id) {
-        case "pauseOrResume":
-          if (!(await onlyAllowedRequester(interaction, player!))) {
-            return;
-          }
-          if (player?.paused) {
-            player?.pause(false);
-            msg = await ruqa.getMessage(
-              interaction.channel.id,
-              interaction.message.id
-            );
-            await msg.edit({
-              components: [
-                {
-                  type: Constants.ComponentTypes.ACTION_ROW,
-                  components: trackStartButtons,
-                },
-              ],
-            });
-            if (ruqa.cacheNpMsgID) {
-              ruqa.cacheNpMsgID.edit({
-                components: [
-                  {
-                    type: Constants.ComponentTypes.ACTION_ROW,
-                    components: nowplayingButtons,
-                  },
-                ],
-              });
-            }
-          } else {
-            player?.pause(true);
-            msg = await ruqa.getMessage(
-              interaction.channel.id,
-              interaction.message.id
-            );
-            msg.edit({
-              components: [
-                {
-                  type: Constants.ComponentTypes.ACTION_ROW,
-                  components: trackStartModifier,
-                },
-              ],
-            });
-            if (ruqa.cacheNpMsgID) {
-              ruqa.cacheNpMsgID.edit({
-                components: [
-                  {
-                    type: Constants.ComponentTypes.ACTION_ROW,
-                    components: nowplayingModifier,
-                  },
-                ],
-              });
-            }
-          }
-          break;
-
-        case "skip":
-          if (!(await onlyAllowedRequester(interaction, player!))) {
-            return;
-          }
-          if (player!.queue.length < 1) {
-            await interaction.createFollowup({
-              embeds: [
-                new RichEmbed()
-                  .setColor(RichEmbed.embedColor)
-                  .setDescription(
-                    "Queue must have at least **1 track** to skip."
-                  ),
-              ],
-              flags: 64,
-            });
-            return;
-          }
-          msg = await ruqa.getMessage(
-            interaction.channel.id,
-            interaction.message.id
-          );
-          player?.skip();
-          await msg.delete();
-          break;
-
-        case "stop":
-          if (!(await onlyAllowedRequester(interaction, player!))) {
-            return;
-          }
-          player?.destroy();
-          msg = await ruqa.getMessage(
-            interaction.channel.id,
-            interaction.message.id
-          );
-          await msg.delete();
-          if (ruqa.cacheNpMsgID) {
-            ruqa.cacheNpMsgID.delete();
-          }
-          break;
-      }
-
-      /**
-       * @description Nowplaying button ID handling
-       * The main reason to create two switch, to split two different stuff.
-       */
-
-      switch (interaction.data.custom_id) {
-        case "np_pauseOrResume":
-          if (!(await onlyAllowedRequester(interaction, player!))) {
-            return;
-          }
-          if (player?.paused) {
-            player?.pause(false);
-            msg = await ruqa.getMessage(
-              interaction.channel.id,
-              interaction.message.id
-            );
-            await msg.edit({
-              components: [
-                {
-                  type: Constants.ComponentTypes.ACTION_ROW,
-                  components: nowplayingButtons,
-                },
-              ],
-            });
-            if (ruqa.cacheMsgID) {
-              ruqa.cacheMsgID.edit({
-                components: [
-                  {
-                    type: Constants.ComponentTypes.ACTION_ROW,
-                    components: trackStartButtons,
-                  },
-                ],
-              });
-            }
-          } else {
-            player?.pause(true);
-            msg = await ruqa.getMessage(
-              interaction.channel.id,
-              interaction.message.id
-            );
-            msg.edit({
-              components: [
-                {
-                  type: Constants.ComponentTypes.ACTION_ROW,
-                  components: nowplayingModifier,
-                },
-              ],
-            });
-            if (ruqa.cacheMsgID) {
-              ruqa.cacheMsgID.edit({
-                components: [
-                  {
-                    type: Constants.ComponentTypes.ACTION_ROW,
-                    components: trackStartModifier,
-                  },
-                ],
-              });
-            }
-          }
-          break;
-
-        case "np_skip":
-          if (!(await onlyAllowedRequester(interaction, player!))) {
-            return;
-          }
-          if (player!.queue.length < 1) {
-            await interaction.createFollowup({
-              embeds: [
-                new RichEmbed()
-                  .setColor(RichEmbed.embedColor)
-                  .setDescription(
-                    "Queue must have at least **1 track** to skip."
-                  ),
-              ],
-              flags: 64,
-            });
-            return;
-          }
-          msg = await ruqa.getMessage(
-            interaction.channel.id,
-            interaction.message.id
-          );
-          ruqa.audio.emit("trackEnd", () => {});
-          player?.skip();
-          await msg.delete();
-          break;
-
-        case "np_refresh":
-          if (!(await onlyAllowedRequester(interaction, player!))) {
-            return;
-          }
-          msg = await ruqa.getMessage(
-            interaction.channel.id,
-            interaction.message.id
-          );
-          const track = player?.current;
-          const [bar, percentage] = buildProgressBar(
-            track?.isStream ? 1 : track?.duration,
-            track?.isStream ? 1 : player?.position
-          );
-
-          const embed = new RichEmbed()
-            .setColor(RichEmbed.embedColor)
-            .setAuthor(
-              player?.current?.title ?? "Unknown title",
-              player?.current?.uri
-            )
-            .setThumbnail(
-              player?.current?.identifier
-                ? `https://img.youtube.com/vi/${player?.current?.identifier}/default.jpg`
-                : ""
-            )
-            .setDescription(
-              `\n\n**Author**: ${player?.current?.author}\n**Requester**: <@!${
-                (player?.current?.requester as User).id
-              }>\n**Duration**: ${prettyMs(Number(player?.current?.duration), {
-                verbose: true,
-              })}`
-            )
-            .addField(
-              "Upcoming",
-              player?.queue[0]
-                ? `[${player.queue[0].title}](${player.queue[0].uri})`
-                : "There are no upcoming track(s) in the queue"
-            )
-            .addField(
-              "Progress",
-              `${bar}\n` +
-                `${
-                  player?.current?.isStream
-                    ? "Stream"
-                    : `${prettyMs(Number(player?.exactPosition), {
-                        verbose: true,
-                      })} (${Number(percentage).toFixed(2)}%)`
-                }`
-            );
-
-          await msg.edit({ embeds: [embed] });
-          break;
-
-        case "np_stop":
-          if (!(await onlyAllowedRequester(interaction, player!))) {
-            return;
-          }
-          player?.destroy();
-          msg = await ruqa.getMessage(
-            interaction.channel.id,
-            interaction.message.id
-          );
-          await msg.delete();
-          if (ruqa.cacheNpMsgID) {
-            ruqa.cacheNpMsgID.delete();
-          }
-          break;
-      }
-
-      /**
-       * @description Queue interaction handler
-       */
-
-      switch (interaction.data.custom_id) {
-        case "q_previousStart":
-          ruqa.page = ruqa.page > 0 ? 0 : ruqa.pages.length - 1;
-          await ruqa.cacheQueueMsg.edit({
-            embeds: [
-              new RichEmbed()
-                .setDescription(`**Queued Tracks**\n\n${ruqa.pages[ruqa.page]}`)
-                .setColor(RichEmbed.embedColor)
-                .setFooter(`Page ${ruqa.page + 1}/${ruqa.pages.length}`),
-            ],
-          });
-          break;
-
-        case "q_previous":
-          ruqa.page = ruqa.page > 0 ? --ruqa.page : ruqa.pages.length - 1;
-          await ruqa.cacheQueueMsg.edit({
-            embeds: [
-              new RichEmbed()
-                .setDescription(`**Queued Tracks**\n\n${ruqa.pages[ruqa.page]}`)
-                .setColor(RichEmbed.embedColor)
-                .setFooter(`Page ${ruqa.page + 1}/${ruqa.pages.length}`),
-            ],
-          });
-          break;
-
-        case "q_forward":
-          ruqa.page = ruqa.page + 1 < ruqa.pages.length ? ++ruqa.page : 0;
-          await ruqa.cacheQueueMsg.edit({
-            embeds: [
-              new RichEmbed()
-                .setDescription(`**Queued Tracks**\n\n${ruqa.pages[ruqa.page]}`)
-                .setColor(RichEmbed.embedColor)
-                .setFooter(`Page ${ruqa.page + 1}/${ruqa.pages.length}`),
-            ],
-          });
-          break;
-
-        case "q_forwardEnd":
-          ruqa.page =
-            ruqa.page + 1 < ruqa.pages.length ? ruqa.pages.length - 1 : 0;
-          await ruqa.cacheQueueMsg.edit({
-            embeds: [
-              new RichEmbed()
-                .setDescription(`**Queued Tracks**\n\n${ruqa.pages[ruqa.page]}`)
-                .setColor(RichEmbed.embedColor)
-                .setFooter(`Page ${ruqa.page + 1}/${ruqa.pages.length}`),
-            ],
-          });
-          break;
-
-        case "q_delete":
-          msg = await ruqa.getMessage(
-            interaction.channel.id,
-            interaction.message.id
-          );
-          await msg.delete();
-          break;
-      }
+  run: async ({ client, message }: { client: Client; message: Message }) => {
+    if (!(await notInVC(message))) {
+      return;
     }
-  }
-);
+
+    const [player, ret] = await isPlayerActive(message);
+    if (!ret) {
+      return;
+    }
+    if (!player!.queue.length) {
+      await buildMsg(message, "There are no tracks in the queue.");
+      return;
+    }
+    const mapping = player?.queue.map(
+      (t, i) =>
+        `\`${++i}.\` [${t.title?.replaceAll("||", "")}](${t.uri}) - [<@!${
+          (t.requester as User).id
+        }>]`
+    );
+    const chunk = makeChunk(mapping!, 10);
+    client.pages = chunk.map((s) => s.join("\n"));
+    client.page = 0;
+    if (client.page) client.page -= 1;
+    if (client.page > client.pages.length) client.page = 0;
+    if (client.page < 0) client.page = 0;
+
+    const embed = new RichEmbed()
+      .setDescription(`**Queued Tracks**\n\n${client.pages[client.page]}`)
+      .setColor(RichEmbed.embedColor)
+      .setFooter(`Page ${client.page + 1}/${client.pages.length}`);
+    client.cacheQueueMsg = await message.channel.createMessage({
+      embeds: [embed],
+      components: [
+        {
+          type: Constants.ComponentTypes.ACTION_ROW,
+          components: queueActiveButtons,
+        },
+      ],
+    });
+  },
+});
