72da003e6f6639920128a00c41b9f3bfbb82bf2a
Ruzie <luhamailbox@gmail.com>
2022-05-08 00:31:55 -0700

    fix(vc): pause the player instead stopping

--- src/listeners/Guild/VoiceChannelLeave.ts
+++ src/listeners/Guild/VoiceChannelLeave.ts
@@ -1,52 +1,48 @@
-import type { Member, VoiceChannel } from "eris";
+import { Constants, type Member, type VoiceChannel } from "eris";
 import Event from "../../structures/Event";
-import ruqa from "../../index";
+import ruqa from "../../index"
 import RichEmbed from "../../utils/RichEmbed";
+import { trackStartModifier, nowplayingModifier } from "../../utils/SetButtons"
 
-export default new Event(
-  "voiceChannelLeave",
-  async (member: Member, oldChannel: VoiceChannel) => {
+export default new Event("voiceChannelLeave", async (member: Member, oldChannel: VoiceChannel) => {
     const player = ruqa.audio.players.get(member.guild.id);
     if (!player) {
-      return;
+        return;
     }
 
     if (ruqa.user.id === member.id) {
-      player.destroy();
+        player.destroy();
     }
 
-    if (
-      !member.bot &&
-      player.voiceChannelId === oldChannel.id &&
-      oldChannel.voiceMembers.filter((x) => x.bot).length
-    ) {
-      player.pause(true);
-      const msg = await ruqa.createMessage(player.textChannelId!, {
-        embeds: [
-          new RichEmbed()
-            .setColor(RichEmbed.embedColor)
-            .setDescription(
-              "All left me alone, I'll take my leave in 10 seconds."
-            ),
-        ],
-      });
-
-      setTimeout(async () => {
-        if (
-          !member.bot &&
-          player.voiceChannelId === oldChannel.id &&
-          oldChannel.voiceMembers.filter((x) => !x.bot).length === 0
-        ) {
-          if (ruqa.cacheMsgID) {
-            await ruqa.cacheMsgID.delete();
-          } else if (ruqa.cacheNpMsgID) {
-            await ruqa.cacheNpMsgID.delete();
-          }
-          player.destroy();
+    if (!member.bot && player.voiceChannelId === oldChannel.id
+    && oldChannel.voiceMembers.filter((x) => x.bot).length) {
+        if (player.paused) {
+            return;
         }
-        player.pause(false);
-        await msg.delete();
-      }, 10e3);
+        player.pause(true);
+        ruqa.cacheLeaveMsg = await ruqa.createMessage(player.textChannelId!, {
+            embeds: [
+                new RichEmbed()
+                .setColor(RichEmbed.embedColor)
+                .setDescription("All left me alone, I'll pause the player."),
+            ],
+        });
+
+        if (ruqa.cacheMsgID) {
+            ruqa.cacheMsgID.edit({
+                components: [{
+                    type: Constants.ComponentTypes.ACTION_ROW,
+                    components: trackStartModifier,
+                }],
+            });
+        }
+        if (ruqa.cacheNpMsgID) {
+            ruqa.cacheNpMsgID.edit({
+                components: [{
+                    type: Constants.ComponentTypes.ACTION_ROW,
+                    components: nowplayingModifier,
+                }],
+            });
+        }
     }
-  }
-);
+});
