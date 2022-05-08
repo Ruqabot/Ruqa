bc6e9b4f109c4dbe22279422b935e17dd78dc9d8
Ruzie <luhamailbox@gmail.com>
2022-05-08 00:24:04 -0700

    add(BuildMsg): now support interaction

--- src/functions/NotInVC.ts
+++ src/functions/NotInVC.ts
@@ -1,12 +1,28 @@
-import type { Message } from "eris";
+import { ComponentInteraction, Message } from "eris";
+import RichEmbed from "../utils/RichEmbed";
 import buildMsg from "./BuildMsg";
 
-export default async function notInVC(message: Message): Promise<boolean> {
+export default async function notInVC(
+  message: Message | ComponentInteraction
+): Promise<boolean> {
   if (!message.member?.voiceState.channelID) {
-    await buildMsg(
-      message,
-      "You need to be in a voice channel before executing this command."
-    );
+    if (message instanceof Message) {
+      await buildMsg(
+        message,
+        "You need to be in a voice channel before executing this command."
+      );
+    } else {
+      await message.createMessage({
+        embeds: [
+          new RichEmbed()
+            .setColor(RichEmbed.embedColor)
+            .setDescription(
+              "You need to be in a voice channel before executing this command."
+            ),
+        ],
+        flags: 64,
+      });
+    }
     return false;
   }
   return true;
