2fd27c1fd972c7f057943d8dd050b8ac0ebd259f
Ruzie <luhamailbox@gmail.com>
2022-05-08 00:33:02 -0700

    🌃 prettier

--- src/listeners/Guild/VoiceChannelLeave.ts
+++ src/listeners/Guild/VoiceChannelLeave.ts
@@ -1,48 +1,58 @@
 import { Constants, type Member, type VoiceChannel } from "eris";
 import Event from "../../structures/Event";
-import ruqa from "../../index"
+import ruqa from "../../index";
 import RichEmbed from "../../utils/RichEmbed";
-import { trackStartModifier, nowplayingModifier } from "../../utils/SetButtons"
+import { trackStartModifier, nowplayingModifier } from "../../utils/SetButtons";
 
-export default new Event("voiceChannelLeave", async (member: Member, oldChannel: VoiceChannel) => {
+export default new Event(
+  "voiceChannelLeave",
+  async (member: Member, oldChannel: VoiceChannel) => {
     const player = ruqa.audio.players.get(member.guild.id);
     if (!player) {
-        return;
+      return;
     }
 
     if (ruqa.user.id === member.id) {
-        player.destroy();
+      player.destroy();
     }
 
-    if (!member.bot && player.voiceChannelId === oldChannel.id
-    && oldChannel.voiceMembers.filter((x) => x.bot).length) {
-        if (player.paused) {
-            return;
-        }
-        player.pause(true);
-        ruqa.cacheLeaveMsg = await ruqa.createMessage(player.textChannelId!, {
-            embeds: [
-                new RichEmbed()
-                .setColor(RichEmbed.embedColor)
-                .setDescription("All left me alone, I'll pause the player."),
-            ],
+    if (
+      !member.bot &&
+      player.voiceChannelId === oldChannel.id &&
+      oldChannel.voiceMembers.filter((x) => x.bot).length
+    ) {
+      if (player.paused) {
+        return;
+      }
+      player.pause(true);
+      ruqa.cacheLeaveMsg = await ruqa.createMessage(player.textChannelId!, {
+        embeds: [
+          new RichEmbed()
+            .setColor(RichEmbed.embedColor)
+            .setDescription("All left me alone, I'll pause the player."),
+        ],
+      });
+
+      if (ruqa.cacheMsgID) {
+        ruqa.cacheMsgID.edit({
+          components: [
+            {
+              type: Constants.ComponentTypes.ACTION_ROW,
+              components: trackStartModifier,
+            },
+          ],
         });
-
-        if (ruqa.cacheMsgID) {
-            ruqa.cacheMsgID.edit({
-                components: [{
-                    type: Constants.ComponentTypes.ACTION_ROW,
-                    components: trackStartModifier,
-                }],
-            });
-        }
-        if (ruqa.cacheNpMsgID) {
-            ruqa.cacheNpMsgID.edit({
-                components: [{
-                    type: Constants.ComponentTypes.ACTION_ROW,
-                    components: nowplayingModifier,
-                }],
-            });
-        }
+      }
+      if (ruqa.cacheNpMsgID) {
+        ruqa.cacheNpMsgID.edit({
+          components: [
+            {
+              type: Constants.ComponentTypes.ACTION_ROW,
+              components: nowplayingModifier,
+            },
+          ],
+        });
+      }
     }
-});
+  }
+);
