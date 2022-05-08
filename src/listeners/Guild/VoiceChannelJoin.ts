64020b3ec0c9b41aae2b7544e2d903f86392c289
Ruzie <luhamailbox@gmail.com>
2022-05-08 00:31:10 -0700

    fix(vc): pause and resume the player if vc 0

---
+++ src/listeners/Guild/VoiceChannelJoin.ts
@@ -0,0 +1,52 @@
+import { Constants, type Member, type VoiceChannel } from "eris";
+import Event from "../../structures/Event";
+import ruqa from "../../index";
+import { trackStartButtons, nowplayingButtons } from "../../utils/SetButtons";
+
+export default new Event(
+  "voiceChannelJoin",
+  async (member: Member, newChannel: VoiceChannel) => {
+    const player = ruqa.audio.players.get(member.guild.id);
+    if (!player) {
+      return;
+    }
+    if (
+      !member.bot &&
+      player.voiceChannelId === newChannel.id &&
+      newChannel.voiceMembers.filter((x) => x.bot).length
+    ) {
+      if (!player.paused) {
+        return;
+      }
+      player.pause(false);
+
+      /**
+       * @description not blocking with "if"
+       */
+      try {
+        await ruqa.cacheLeaveMsg.delete();
+      } catch {}
+
+      if (ruqa.cacheMsgID) {
+        ruqa.cacheMsgID.edit({
+          components: [
+            {
+              type: Constants.ComponentTypes.ACTION_ROW,
+              components: trackStartButtons,
+            },
+          ],
+        });
+      }
+      if (ruqa.cacheNpMsgID) {
+        ruqa.cacheNpMsgID.edit({
+          components: [
+            {
+              type: Constants.ComponentTypes.ACTION_ROW,
+              components: nowplayingButtons,
+            },
+          ],
+        });
+      }
+    }
+  }
+);
